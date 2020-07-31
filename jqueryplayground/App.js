826487d4a87e61c60095399a0175f5f864b2afeb
Chanrich Pisitjing <vjumpkung@gmail.com>
2020-08-01 00:36:30 +0700

    fun

---
+++ jqueryplayground/App.js
@@ -0,0 +1,13 @@
+
+  var serverTime = new Date();
+
+  function updateTime() {
+      /// Increment serverTime by 1 second and update the html for '#time'
+      serverTime = new Date(serverTime.getTime() + 1000);
+      $('#time').html(serverTime.toLocaleTimeString());
+  }
+  $(function() {
+      updateTime();
+      setInterval(updateTime, 1000);
+  });
+
